91eeafa1bb6500ed8d3701e1a62d04e012ab519b
Lev Velykoivanenko <velykoivanenko.lev@gmail.com>
2025-10-25 16:00:00 +0200

    ci(python): add injection test file

---
+++ tests/query/injections/python/regex.py
@@ -0,0 +1,16 @@
+import re
+
+re_test = re.compile(r"^(?P<year>\d{4}) (?P<day>\d) \w\s{,3}$")
+#                                    ^ @regex
+re_test = re.compile(
+    # comment
+    # ^ @comment
+    r"^(?P<year>\d{4}) "
+    # comment
+    # ^ @comment
+    r"(?P<day>\d) \w\s{,3}$"
+    # ^ @regex
+    # comment
+    # comment
+    # ^ @comment
+)
